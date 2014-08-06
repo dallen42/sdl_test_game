37154096dc3cc3930c5b6d6c36c57d3476680cbb
David Allen <dallen42@users.noreply.github.com>
2014-08-06 16:19:55 -0500

    Hello World

--- main.cpp
+++ main.cpp
@@ -15,6 +15,8 @@ int main( int argc, char* args[] )
     mainWindow = SDL_CreateWindow( "SDL basic test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1980, 1080, SDL_WINDOW_FULLSCREEN );
     screen = SDL_GetWindowSurface( mainWindow );
 
+    std::cout<<"Hello World SDL!!!!!"<<std::endl;
+
     while(!done)
     {
         while(SDL_PollEvent(&event)!=0)
