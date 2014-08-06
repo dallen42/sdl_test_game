1eaa7fbc873ce4547ab770ede08e2a8149552ff1
dallen42 <dallen22g@gmail.com>
2014-08-06 15:38:31 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,39 @@
+
+
+#include <stdio.h>
+#include <iostream>
+
+#include <SDL.h>
+
+
+int main( int argc, char* args[] )
+{
+    SDL_Window* mainWindow = NULL;
+    SDL_Surface* screen = NULL;
+    SDL_Event event;
+    bool done = false;
+    mainWindow = SDL_CreateWindow( "SDL basic test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1980, 1080, SDL_WINDOW_FULLSCREEN );
+    screen = SDL_GetWindowSurface( mainWindow );
+
+    while(!done)
+    {
+        while(SDL_PollEvent(&event)!=0)
+        {
+            if(event.type == SDL_KEYDOWN )
+            {
+                switch(event.key.keysym.sym)
+                {
+                    case SDLK_ESCAPE:
+                        done = true;
+                }
+            }
+        }
+        SDL_FillRect( screen, NULL, SDL_MapRGB( screen->format, 0x00, 0x00, 0xF0 ) );
+        SDL_UpdateWindowSurface( mainWindow );
+    }
+
+    SDL_DestroyWindow( mainWindow );
+    SDL_Quit();
+
+    return 0;
+}
